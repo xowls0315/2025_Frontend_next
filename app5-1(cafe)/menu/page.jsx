b9010368deb8a1b846f3fc274937842d02cb2e43
t.jin_01 <155832601+xowls0315@users.noreply.github.com>
2025-05-02 16:11:03 +0900

    Add files via upload

--- app5-1(cafe)/menu/page.jsx
+++ app5-1(cafe)/menu/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { animation } from "@/styles/motion";
 import Container from "@/components/common/layout/Container";
 import Banner from "@/components/menu/Banner";
 import Beer from "@/components/menu/Beer";
@@ -9,50 +11,59 @@ import Selector from "@/components/menu/Selector";
 import Wine from "@/components/menu/Wine";
 import Link from "next/link";
 
+const componentMap = {
+  커피: {
+    Component: Coffee,
+    urlPrefix: "coffee",
+    api: "https://api.sampleapis.com/coffee/hot",
+  },
+  맥주: {
+    Component: Beer,
+    urlPrefix: "beer",
+    api: "https://api.sampleapis.com/beers/ale",
+  },
+  와인: {
+    Component: Wine,
+    urlPrefix: "wine",
+    api: "https://api.sampleapis.com/wines/reds",
+  },
+};
+
 const Page = () => {
   const [selected, setSelected] = useState("커피");
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      let url = "";
-      if (selected === "커피") url = "https://api.sampleapis.com/coffee/hot";
-      else if (selected === "맥주")
-        url = "https://api.sampleapis.com/beers/ale";
-      else if (selected === "와인")
-        url = "https://api.sampleapis.com/wines/reds";
-
+      const url = componentMap[selected].api;
       const res = await fetch(url);
       const json = await res.json();
-      setData(json.slice(0, 8)); // 예시로 8개
+      setData(json.slice(0, 8));
     };
 
     fetchData();
   }, [selected]);
+
+  const { Component, urlPrefix } = componentMap[selected];
 
   return (
     <>
       <Banner selected={selected} />
       <Selector selected={selected} setSelected={setSelected} />
       <Container className="grid grid-cols-4 gap-10 py-10 px-8 md:px-0">
-        {selected === "커피" &&
-          data.map((v, i) => (
-            <Link key={i} href={`/menu/coffee/${v.id}`}>
-              <Coffee {...v} />
+        {data.map((v, i) => (
+          <motion.div
+            key={i}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={animation.fadeInSlideUp}
+          >
+            <Link href={`/menu/${urlPrefix}/${v.id}`}>
+              <Component {...v} />
             </Link>
-          ))}
-        {selected === "맥주" &&
-          data.map((v, i) => (
-            <Link key={i} href={`/menu/beer/${v.id}`}>
-              <Beer {...v} />
-            </Link>
-          ))}
-        {selected === "와인" &&
-          data.map((v, i) => (
-            <Link key={i} href={`/menu/wine/${v.id}`}>
-              <Wine {...v} />
-            </Link>
-          ))}
+          </motion.div>
+        ))}
       </Container>
     </>
   );
